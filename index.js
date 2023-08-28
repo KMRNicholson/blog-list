2e3ebb471e2d3870ce6ff230934992865a08790e
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-08-28 16:19:05 +0300

    refact[4.2]: move config into own utility file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
 const express = require('express')
-
-const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-const mongoUrl = process.env.MONGODB_URI
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 const blogRouter = require('./controllers/blogs')
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(blogRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const port = config.PORT
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
