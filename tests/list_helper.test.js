d8edf7b4eaec2240f9f9a03c38cd591ab8e75b0b
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-08-28 19:42:26 +0300

    test[4.5]: add test for favoriteBlog function

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,5 +1,31 @@
 const listHelper = require('../utils/list_helper')
-const blogs = require('./data/blogs')
+
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 1,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 3,
+    __v: 0
+  }
+]
 
 test('dummy function returns one', () => {
   const result = listHelper.dummy([])
@@ -22,10 +48,30 @@ describe('total likes', () => {
   })
 
   test('of a bigger list is calculated correctly', () => {
-    const sum = (accumulator, currentValue) => accumulator + currentValue
-    const expected = blogs
-      .map(blog => blog.likes)
-      .reduce(sum, 0)
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(6)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog, equals that blog', () => {
+    const listWithOneBlog = [
+      blogs[0]
+    ]
+
+    const expected = { ...blogs[0] }
+
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(expected)
+  })
+
+  test('of a list with one clear favorite returns that favorite', () => {
+    const expected = blogs[2]
 
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(expected)
