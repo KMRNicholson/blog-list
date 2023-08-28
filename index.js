df1970e3e5d06d3f25e22d364ae034f9858095c4
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-08-28 15:16:09 +0300

    refact[4.2]: move logic from index.js to blogs controller and model

--- index.js
+++ index.js
@@ -5,46 +5,15 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
+
+const blogRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.get('/api/blogs/:id', (request, response) => {
-  Blog
-    .findById(request.params.id)
-    .then(blog => {
-      response.json(blog)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
