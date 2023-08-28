e35a1a58c2942c86dd62e07678c56ea989c0a450
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-08-28 18:34:39 +0300

    test[4.4]: add totalLikes function with tests

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,9 +1,33 @@
 const listHelper = require('../utils/list_helper')
+const blogs = require('./data/blogs')
 
-test('dummy function returns 1', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-
+test('dummy function returns one', () => {
+  const result = listHelper.dummy([])
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [
+      blogs[0]
+    ]
+
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(blogs[0].likes)
+  })
+
+  test('of a bigger list is calculated correctly', () => {
+    const sum = (accumulator, currentValue) => accumulator + currentValue
+    const expected = blogs
+      .map(blog => blog.likes)
+      .reduce(sum, 0)
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(expected)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,21 @@
 const dummy = blogs => 1
 
+const totalLikes = blogs => {
+  let result = 0
+
+  if (blogs.length === 1) result = blogs[0].length
+
+  if (blogs.length >= 1) {
+    const sum = (accumulator, currentValue) => accumulator + currentValue
+    result = blogs
+      .map(blog => blog.likes)
+      .reduce(sum, 0)
+  }
+
+  return result
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
