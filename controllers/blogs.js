172a86fa0788d04ebdc50f96166cc9dd3c6743dd
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-09-02 00:05:54 +0300

    refact[4.*]: refactor get /api/blogs/:id to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,7 @@ const Blog = require('../models/Blog')
 
 blogsRouter.get('/api/blogs', async (request, response) => { response.send(await Blog.find({})) })
 
-blogsRouter.get('/api/blogs/:id', (request, response, next) => {
-  Blog
-    .findById(request.params.id)
-    .then(blog => {
-      response.json(blog)
-    })
-    .catch(error => next(error))
-})
+blogsRouter.get('/api/blogs/:id', async (request, response) => { response.json(await Blog.findById(request.params.id)) })
 
 blogsRouter.post('/api/blogs', async (request, response) => {
   const blog = new Blog(request.body)
