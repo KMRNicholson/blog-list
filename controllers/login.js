231c5c2d3e21cb5f9fb19aed78c5c1e11fc3f980
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-09-03 16:33:04 +0300

    feat[4.18]: add login controller to code

---
+++ controllers/login.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/User')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+  const obfuscatedMessage = 'invalid username or password'
+
+  const user = await User.findOne({ username })
+  if (user === null) response.status(404).send(obfuscatedMessage)
+
+  const correctPassword = await bcrypt.compare(password, user.passwordHash)
+  if (correctPassword) {
+    response.status(401).send(obfuscatedMessage)
+  }
+
+  const userInfo = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userInfo, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
